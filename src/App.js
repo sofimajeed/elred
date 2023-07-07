771941ee51441eda1d1f12dfc466cb6092def2f0
Sofi Majeed <majeed.sofi@jungleworks.com>
2023-07-08 00:13:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Button } from 'reactstrap';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="input-group mb-3">
+  <span className="input-group-text  bg-info" id="basic-addon1">Start</span>
+</div>
     </div>
   );
 }
